6b1a5df67bb2df8d0352ecd1b0c86391787fb141
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-07-24 15:46:12 +0100

    Add RegisterPage component to render RegisterForm

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/RegisterForm";
+
+export const metadata = {
+  title: "Register - WarehouseOS",
+};
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
